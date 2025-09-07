4a7880261d94f5b603e8af3085e89336bd90901a
Kandrianaha <k.andria@icloud.com>
2025-09-07 14:39:58 +1000

    fixing code

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,7 @@
 add = __import__('add_0').add
 
 if __name__ == "__main__":
-    import add_0
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
