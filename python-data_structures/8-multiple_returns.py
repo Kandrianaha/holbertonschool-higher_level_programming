9407cb045d04f80423b07d583833370c4125d976
Kandrianaha <k.andria@icloud.com>
2025-09-10 15:45:32 +1000

    multiple returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return None, None
-    return len(sentence), sentence[0]
+    length = len(sentence)
+    first = sentence[0] if length > 0 else None
+    print("Length: {:d} - First character: {}".format(length, first))
+    return (length, first)
