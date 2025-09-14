5f2c8707f3b64f6fa2ea429705ad6c923675b24e
Kandrianaha <k.andria@icloud.com>
2025-09-14 20:35:15 +1000

    test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return a + b
