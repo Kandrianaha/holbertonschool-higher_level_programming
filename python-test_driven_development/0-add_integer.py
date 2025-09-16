c391579d059055dd4063c773f11a55ba25dc48d1
Kandrianaha <k.andria@icloud.com>
2025-09-16 20:57:17 +1000

    adding module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,8 +11,8 @@ def add_integer(a, b=98):
     Floats are truncated to integers before addition.
     Returns the sum of a and b as an integer.
     """
-    if not isinstance(a, int):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return a + b
