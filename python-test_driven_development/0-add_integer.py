e36adc19c42c1832a2e3e5951e0f2885378d7b8d
Kandrianaha <k.andria@icloud.com>
2025-09-16 20:46:39 +1000

    adding module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+This module docstring provides a function to add two integers after validating their types.
+If the inputs are integers or floats, then cast floats to integers,
+and raise appropriate errors if the inputs are invalid.
+"""
 def add_integer(a, b=98):
+    """
+    Adds two integers or floats after converting them to integers.
+    Both a and b must be integers or floats, otherwise a TypeError is raised.
+    Floats are truncated to integers before addition.
+    Returns the sum of a and b as an integer.
+    """
     if not isinstance(a, int):
         raise TypeError("a must be an integer")
     if not isinstance(b, int):
