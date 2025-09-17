e36b8ffd2f74e56103dcb226fe362d41644f1b06
Kandrianaha <k.andria@icloud.com>
2025-09-17 21:01:46 +1000

    fixing error in matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,13 +27,13 @@ def matrix_divided(matrix, div):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
 
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError("matrix must have each row with the same size")
-
+    
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
