6e1011d1909fbcb3de01ef88b8bcb1106fc078a1
Kandrianaha <k.andria@icloud.com>
2025-09-17 11:14:52 +1000

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-This module docstring provides a function to add two integers after validating their types.
+This module docstring provides a function
+    to add two integers after validating their types.
 If the inputs are integers or floats, then cast floats to integers,
 and raise appropriate errors if the inputs are invalid.
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two integers or floats after converting them to integers.
@@ -18,5 +21,5 @@ def add_integer(a, b=98):
 
     a = int(a)
     b = int(b)
-    
+
     return a + b
