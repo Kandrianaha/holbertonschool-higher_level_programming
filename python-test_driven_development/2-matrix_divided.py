e218df50b660346126af1aef2d7ba3bf7a1e7c16
Kandrianaha <k.andria@icloud.com>
2025-09-17 20:53:42 +1000

    fixing module

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by a given divisor.
+    """
+    Divides all elements of a matrix by a given divisor.
 
     Args:
         matrix (list of lists of int/float): The matrix to be divided.
