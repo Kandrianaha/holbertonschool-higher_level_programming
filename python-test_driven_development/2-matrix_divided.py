b8b4993d7799ea4b73bf608800ca3a62c84970c1
Kandrianaha <k.andria@icloud.com>
2025-09-17 21:03:47 +1000

    fixing error in matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -30,7 +30,7 @@ def matrix_divided(matrix, div):
 
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
-        raise TypeError("matrix must have each row with the same size")
+        raise TypeError("Each row of the matrix must have the same size")
     
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
