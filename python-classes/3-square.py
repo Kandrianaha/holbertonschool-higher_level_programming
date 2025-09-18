f6fdb1cdb72d059ae92783ba3aee23db4fd2ce08
Kandrianaha <k.andria@icloud.com>
2025-09-18 19:55:30 +1000

    size private attribute for Square otherwise TypeError

---
+++ python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module provides a function that defines a Square by private instance
+attribute size.
+"""
+
+class Square:
+    """
+    A class that defines a square by private instance attribute size.
+
+    Attributes:
+        size (int): The size of the square's sides.
+
+    Methods:
+        __init__(self, size=0): Initializes the square with a given size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the square with a given size.
+
+        Args:
+            size (int): The size of the square's sides. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
