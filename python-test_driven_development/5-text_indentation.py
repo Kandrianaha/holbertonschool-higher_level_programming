67f732dadd42334d7c22813fc926517da0203e6f
Kandrianaha <k.andria@icloud.com>
2025-09-18 18:08:21 +1000

    pycodestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,7 @@ This module provides a function that prints a text
 with 2 new lines after each of these characters: ., ? and :
 """
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
