b5dcd193ec9f3c0af8d04afad6a282072ae0658a
Kandrianaha <k.andria@icloud.com>
2025-09-18 19:39:19 +1000

    square attribute = size

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides a class that defines a square by its size.
+"""
+
+class Square:
+    """
+    A class to represent a square.
+
+    Attributes:
+        None
+    """
+
+size = 0
+    pass
