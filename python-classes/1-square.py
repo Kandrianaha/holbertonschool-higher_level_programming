6135d4949d80506f45916d287aba0966c773308c
Kandrianaha <k.andria@icloud.com>
2025-09-18 19:45:18 +1000

    square attribute = size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-This module provides a class that defines a square by its size.
+This module provides a class Square that defines a square by its size.
 """
 
 
 class Square:
     """
-    This class defines a square by its size.
+    This class defines Square by its size.
 
     Attributes:
-        None
+        __size (int): The size of the square.
     """
 
 def __init__(self, size):
