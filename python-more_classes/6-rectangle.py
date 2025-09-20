54b4a89249fb7f6e0bf7456037c30d548596b320
Kandrianaha <k.andria@icloud.com>
2025-09-20 16:38:49 +1000

    pycodestyle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -67,7 +67,8 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Returns a string representation of the rectangle using '#' characters."""
+        """Returns a string representation of the rectangle
+        using '#' characters."""
         if self.width == 0 or self.height == 0:
             return ""
         return '\n'.join(['#' * self.width for _ in range(self.height)])
