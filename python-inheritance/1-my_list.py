0879190fd568371e335787769a4b4e64450515e1
Kandrianaha <k.andria@icloud.com>
2025-09-27 05:23:50 +0000

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module class MyList inherits from list.
+"""
+
+
+class MyList(list)
+    def print_sorted(self):
+    """
+    Prints the list in ascending sorted order.
+    """
+    print(sorted(self)
