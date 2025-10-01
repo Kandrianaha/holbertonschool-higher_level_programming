b15ac657fcd9926c1fcb1323652c45aedb7c4170
Kandrianaha <k.andria@icloud.com>
2025-10-01 18:26:48 +1000

    empty class BaseGeomettry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module writes an emptty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing basic geometric shapes.
+    """
+    pass
