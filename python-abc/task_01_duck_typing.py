be936a34f8b185cefb9e003b7c542704465a8926
Kandrianaha <k.andria@icloud.com>
2025-10-04 15:51:24 +1000

    Shape

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -38,6 +38,8 @@ class Circle(Shape):
         Args:
             radius (float): The radius of the circle.
         """
+        if radius < 0:
+            ValueError("Radius cannot be negative.")
         self.radius = radius
 
     def area(self):
