5523974382fc0db8211b5129d494302f660170f7
Kandrianaha <k.andria@icloud.com>
2025-10-05 14:17:41 +1100

    flying fish

--- python-abc/main_04_flyingfish.py
+++ python-abc/main_04_flyingfish.py
@@ -1,38 +1,7 @@
-#!/usr/bin/python3
-"""
-This module construct a FlyingFish class
-that inherits from both Fish and Bird classes.
-"""
-
-
-class Fish:
-    """Fish class with swim method."""
-
-    def swim(self):
-        """Simulate swimming action."""
-        print("The fish is swimming.")
-
-    def habitat(self):
-        """Return the habitat of the fish."""
-        print("The fish lives in water.")
-
-class Bird:
-    def fly(self):
-        """Simulate flying action."""
-        print("The bird is soaring.")
-    
-    def habitat(self):
-        """Return the habitat of the bird."""
-        print("The bird lives in tthe sky.")
-
-class FlyingFish(Fish, Bird):
-    def habitat(self):
-        """Return the habitat of the flying fish."""
-        print("The flying fish lives in both water and the sky.")
+#!/usr/bin/env python3
+from task_04_flyingfish import Fish, FlyingFish
 
 flying_fish = FlyingFish()
-flying_fish.swim()  # Inherited from Fish
-flying_fish.fly()   # Inherited from Bird
-flying_fish.habitat()  # Overridden method
-
-print(FlyingFish.mro())  # Display the method resolution order
+flying_fish.swim()
+flying_fish.fly()
+flying_fish.habitat()
