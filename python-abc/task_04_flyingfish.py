5158b57281a0b11803e5f2f82a9fd503b6122c58
Kandrianaha <k.andria@icloud.com>
2025-10-05 14:55:37 +1100

    flying fish

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -19,13 +19,21 @@ class Fish:
 class Bird:
     def fly(self):
         """Simulate flying action."""
-        print("The bird is soaring.")
+        print("The bird is flying.")
     
     def habitat(self):
         """Return the habitat of the bird."""
-        print("The bird lives in tthe sky.")
+        print("The bird lives in the sky.")
 
 class FlyingFish(Fish, Bird):
+    def fly(self):
+        """Overrrides the fly method."""
+        print("The flying fish is soaring!.")
+
+    def swim(self):
+        """Overrides the swim method."""
+        print("The flying fish is swimming!")
+
     def habitat(self):
         """Return the habitat of the flying fish."""
         print("The flying fish lives in both water and the sky.")
@@ -35,4 +43,4 @@ flying_fish.swim()  # Inherited from Fish
 flying_fish.fly()   # Inherited from Bird
 flying_fish.habitat()  # Overridden method
 
-print(FlyingFish.mro())  # Display the method resolution order
+print(FlyingFish.mro)  # Display the method resolution order
