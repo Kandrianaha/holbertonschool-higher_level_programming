2b0808fc95145ec7a1000e9ec61aa19c7b69c627
Kandrianaha <k.andria@icloud.com>
2025-10-05 14:58:25 +1100

    flying fish

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -32,7 +32,7 @@ class FlyingFish(Fish, Bird):
 
     def swim(self):
         """Overrides the swim method."""
-        print("The flying fish is swimming!")
+        print("The flying fish is swimming!.")
 
     def habitat(self):
         """Return the habitat of the flying fish."""
