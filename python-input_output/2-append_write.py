cab3cbdcfdb80356966892d5d4bdeb08e55efbcd
Kandrianaha <k.andria@icloud.com>
2026-02-18 19:37:31 +1100

    pycodestyel

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""This module appends a string to a text file (UTF8) and returns the number of characters added."""
+"""This module appends a string to a text file (UTF8)"""
+
 
 def append_write(filename="", text=""):
-    """Append a string at the end of a text file (UTF8) and return the number of characters added"""
+    """Append a string at the end of a text file (UTF8)"""
+
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
