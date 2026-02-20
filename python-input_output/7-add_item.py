f261e084947991e6e0c8f19247ef73d325a4ea92
Kandrianaha <k.andria@icloud.com>
2026-02-20 21:12:11 +1100

    fixing code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,14 +4,17 @@ all arguments to a Python list,
 and then save them to a file"""
 
 import sys
-import json
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arguments = sys.argv[1:]
 filename = "add_item.json"
-with open(filename, 'a+', encoding='utf-8') as f:
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
     my_list = []
-    my_list.append(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
