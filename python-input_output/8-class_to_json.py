1e9ad1585b5978e867edfe37d8bcb2b679cc502c
Kandrianaha <k.andria@icloud.com>
2026-02-20 21:19:02 +1100

    adding line above def class

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,6 +3,7 @@
 description with simple data structure (list, dictionary,
 string, integer and boolean) for JSON serialization of an object"""
 
+
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean)
