24af9c11d0a6cac59f357706f2e2675338b79285
Kandrianaha <k.andria@icloud.com>
2026-02-20 21:54:42 +1100

    student to JSON

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Student that defines a student by
+Public instance attributes: first_name, last_name, age"""
+
+class Student:
+    """Class Student defined by first_name, last_name, age"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first_name, last_name, age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the JSON representation of the Student instance"""
+        if attrs is None:
+            return self.__dict__
+        return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+
