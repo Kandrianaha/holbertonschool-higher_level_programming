25c1ab71cc90f3f022b914ef7528c2da6e52c7e1
Kandrianaha <k.andria@icloud.com>
2026-02-20 20:56:13 +1100

    add item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+"""This module writes a script that adds
+all arguments to a Python list,
+and then save them to a file"""
+
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+arguments = sys.argv[1:]
+filename = "add_item.json"
+with open(filename, 'a+', encoding='utf-8') as f:
+    my_list = []
+    my_list.append(arguments)
+    save_to_json_file(my_list, filename)
+    load_from_json_file(filename)
