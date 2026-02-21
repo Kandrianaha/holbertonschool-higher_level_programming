c97d59a99ee2ac06bb29881984c2abf5b6297a63
Kandrianaha <k.andria@icloud.com>
2026-02-21 12:37:15 +1100

    CSV data to JSON format

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import csv
+import json
+
+def convert_csv_to_json(csv_filename):
+    try:
+        with open(csv_filename, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+
+        with open(json_filename, 'w') as json_file:
+            json.dump(data, json_file)
+
+        return True
+    
+    except FileNotFoundError:
+        return False
