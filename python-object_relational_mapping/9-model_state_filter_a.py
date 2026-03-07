3c3119c75280abca21fb534635114a12ce4e5541
Kandrianaha <k.andria@icloud.com>
2026-03-07 21:38:30 +1100

    every object with a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name.like('%a%')
+    ).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
     session.close()
