84a6de2d2befbba5deba151b0e6f430441e4969b
Kandrianaha <k.andria@icloud.com>
2026-03-07 18:33:44 +1100

    fixing line 17 too long

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+    )
     rows = cursor.fetchall()
     for row in rows:
         print(row)
