6f050fd73e5b9744f84e41965ed60e109fa38505
Kandrianaha <k.andria@icloud.com>
2026-03-07 18:56:29 +1100

    filter states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,18 +4,26 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Get MySQL credentials and database name from arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
+
+    # Create a cursor
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE BINARY name = '{}' "
-        "ORDER BY id ASC".format(sys.argv[4])
-    )
+        "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC",
+        (state_name_searched,))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
