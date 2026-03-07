34c4033d85d59c919ebb61ebb4dc2979d87ff400
Kandrianaha <k.andria@icloud.com>
2026-03-07 22:06:04 +1100

    fixing code

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,6 +7,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
@@ -24,6 +27,6 @@ if __name__ == "__main__":
         .all()
 
     for state, city in results:
-        print("{}: {} -> {}".format(city.id, city.name, state.name))
+        print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
