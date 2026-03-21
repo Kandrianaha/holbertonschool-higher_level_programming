da4f470d325af516db7d9182470fadd407c4fb04
Kandrianaha <k.andria@icloud.com>
2026-03-22 09:33:21 +1100

    Creating a Simple Templating Program

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -21,5 +21,5 @@ def generate_invitations(template, attendees):
         content = content.replace("{event_date}", str(attendee.get("event_date", "N/A") or "N/A"))
         content = content.replace("{event_location}", str(attendee.get("event_location", "N/A") or "N/A"))
 
-    with open(f"output_{i}.txt", "w") as f:
+    with open(f"output_01.txt", "w") as f:
         f.write(content)
