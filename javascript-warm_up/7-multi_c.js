97696cca1cd97f5989c6d184ef4f6a679e269361
Kandrianaha <k.andria@icloud.com>
2026-03-21 20:19:09 +1100

    fixing indentation to 2 instead of 4

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ const x = process.argv[2];
 const num = parseInt(x);
 
 if (isNaN(num)) {
-    console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
-    for (let i = 0; i < num; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
