17499d08d9215a167342fbf3d84bd7b5e27986ad
Kandrianaha <k.andria@icloud.com>
2026-03-21 21:03:52 +1100

    adding 'red' class

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-document.querySelector('#red_header').addEventListered('click', function()) {
+document.querySelector('#red_header').addEventListener('click', function () {
   document.querySelector('header').style.color = '#FF0000';
-};
+});
